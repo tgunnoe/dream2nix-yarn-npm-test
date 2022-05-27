e5608674a8a566da19d7a4129581c69cffafe7d4
tgunnoe <t@gvno.net>
2022-05-27 12:36:03 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.dream2nix.url = "github:nix-community/dream2nix";
+  outputs = { self, dream2nix }@inputs:
+    let
+      dream2nix = inputs.dream2nix.lib.init {
+        systems = [ "x86_64-linux" ];
+        config.projectRoot = ./.;
+      };
+    in
+      dream2nix.makeFlakeOutputs {
+        pname = "test-npm";
+        source = ./.;
+      };
+
+}
