1cd2d748f235fd29ef821bd5e290adacd3fb6f72
tgunnoe <t@gvno.net>
2022-05-27 16:00:36 -0400

    Initial commit 2

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
-  inputs.dream2nix.url = "github:nix-community/dream2nix";
+  #inputs.dream2nix.url = "github:nix-community/dream2nix";
+  inputs.dream2nix.url = "path:/home/tgunnoe/src/boba/dream2nix";
   outputs = { self, dream2nix }@inputs:
     let
       dream2nix = inputs.dream2nix.lib.init {
